674bf76c113a17c1629cacb13f4bb750592c6279
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2021-03-16 13:28:46 +0200

    Update and rename 1.c to 2 Лабораторна 3 завдання.c

---
+++ 2 Лабораторна 3 завдання.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+int main(void) {
+int a,b,sy,fc,d; 
+setlocale (LC_CTYPE,"ua");
+printf("\n Перше число a = ");
+		scanf("%d",&a);
+printf("\n Друге число b = ");
+	    scanf("%d",&b);
+sy=a+b;
+printf("\n\r\tСума чисел = %d\n",sy);
+fc=(a/b);
+printf("\n\r\tЦіла частина від ділення 1 на 2 = %d\n",fc);
+d=(a+b)*3;
+printf("\n\r\tПотроєний добуток = %d\n\n",d);
+	
+	return 0;
+}
