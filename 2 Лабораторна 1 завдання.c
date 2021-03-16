bdaaeb1c8d0360e718070a94623bc1199b52ac76
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2021-03-16 15:07:48 +0200

    Update 2 Лабораторна 1 завдання.c

--- 2 Лабораторна 1 завдання.c
+++ 2 Лабораторна 1 завдання.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 #include<math.h>
 #include<locale.h>
-int main(void) {
-	int a,b,r,sa,c;
+void main(void) {
+	float a,b,r,sa,c;
 setlocale (LC_CTYPE,"ua");
 printf("\n Перше число a = ");
-		scanf("%d",&a);
+		scanf("%f",&a);
 printf("\n Друге число b = ");
-	    scanf("%d",&b);
+	    scanf("%f",&b);
  r = a-b;
-printf("\n\r\t Різниця чисел = %d\n",r);
+printf("\n\r\tРiзниця чисел = %.2f\n",r);
  sa = (a+b)/2;
-printf("\r\t Середнє арифметичне = %d\n",sa);
+printf("\r\tСереднє арифметичне = %.2f\n",sa);
  c = a+b;
-printf("\r\t Сума чисел = %d\n\n",c);
-	return 0;
+printf("\r\tСума чисел = %.2f\n\n",c);
 	}
