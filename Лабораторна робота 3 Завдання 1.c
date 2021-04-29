308d157d50b1b11be8edd242911816e10cf70567
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2021-04-29 10:58:55 +0300

    Update and rename Z1.c to Лабораторна робота 3 Завдання 1.c

--- Лабораторна робота 3 Завдання 1.c
+++ Лабораторна робота 3 Завдання 1.c
@@ -5,11 +5,11 @@ int main(void) {
 setlocale(LC_CTYPE,"ua");
 int x,y,b;
 float a;
-printf("\n\t Задайте x = ");
+printf("\n\t Р—Р°РґР°Р№С‚Рµ x = ");
 scanf("%d", &x);
-printf("\n\t Задайте y = ");
+printf("\n\t Р—Р°РґР°Р№С‚Рµ y = ");
 scanf("%d", &y);
-printf("\n\t Задайте b = ");
+printf("\n\t Р—Р°РґР°Р№С‚Рµ b = ");
 scanf("%d", &b);
 	if(b<0)a =(float) b+sqrt(x-4*y);
 	else if(b>7)a =(float)(b*y)/(2*y-3*x)-4;
