630ca41c16bebab510a73eefc1526864def0f9ea
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2021-05-05 12:39:12 +0300

    Update and rename z2.c to 5 Лабораторна 2 завдання А.c

--- 5 Лабораторна 2 завдання А.c
+++ 5 Лабораторна 2 завдання А.c
@@ -7,9 +7,9 @@ int n=1;int kol =0; int ind1 = 0; int ind2 = 0;int maximus = 0;int max = 0;
 int sum = 0;
 setlocale(LC_CTYPE,"ua");
 while(1){ 
-printf("\n\nВведiть кiлькiсть масивiв: ");
+printf("\n\nР’РІРµРґiС‚СЊ РєiР»СЊРєiСЃС‚СЊ РјР°СЃРёРІiРІ: ");
 scanf("%d",&n);
-if(n<=0){printf("\n\nВведiть число бiльше нуля...\n");continue;}
+if(n<=0){printf("\n\nР’РІРµРґiС‚СЊ С‡РёСЃР»Рѕ Р±iР»СЊС€Рµ РЅСѓР»СЏ...\n");continue;}
 
 else{ 
 int i = n;int colCount = n;
@@ -24,13 +24,13 @@ max = a[i];
 maximus = i; }}
 
 
-printf("Max масивiв: a[%d] = %d",maximus, a[maximus]);
+printf("Max РјР°СЃРёРІiРІ: a[%d] = %d",maximus, a[maximus]);
 for(int i = 0; i < colCount; i++){
     if(a[i]==0 && kol==0){
         kol++;
         ind1 = i;
     continue;}
-    if(kol==0){printf("\nНулiв немає");break;}
+    if(kol==0){printf("\nРќСѓР»iРІ РЅРµРјР°С”");break;}
 if(a[i]==0 && kol == 1){
     kol++;
         ind2 = i;
@@ -40,11 +40,11 @@ if (a[i]==0 && kol == 2){
     kol = kol + 1;
 continue;}
 if(kol == 3){
-    printf(" 3 нуля. добуток = 0");break;
+    printf(" 3 РЅСѓР»СЏ. РґРѕР±СѓС‚РѕРє = 0");break;
 }
 }
 if((ind1 + 1) == ind2){
-    printf("Нулi стоять дуже близько i заданi масиви не можуть відповiдати умовам задачi");
+    printf("РќСѓР»i СЃС‚РѕСЏС‚СЊ РґСѓР¶Рµ Р±Р»РёР·СЊРєРѕ i Р·Р°РґР°РЅi РјР°СЃРёРІРё РЅРµ РјРѕР¶СѓС‚СЊ РІС–РґРїРѕРІiРґР°С‚Рё СѓРјРѕРІР°Рј Р·Р°РґР°С‡i");
     break;
 }
 if(kol == 2){
@@ -52,10 +52,10 @@ if(kol == 2){
     for (int i = ind1 + 1; i < ind2; i++){ 
             sum = sum + a[i];
       }
-      printf("\n Сума мiж нулями:  %d\n",sum);
+      printf("\n РЎСѓРјР° РјiР¶ РЅСѓР»СЏРјРё:  %d\n",sum);
 break;}
 if(kol==1){
-    printf("Всього лиш один нуль. Не вiдповiдає умовам задачi");
+    printf("Р’СЃСЊРѕРіРѕ Р»РёС€ РѕРґРёРЅ РЅСѓР»СЊ. РќРµ РІiРґРїРѕРІiРґР°С” СѓРјРѕРІР°Рј Р·Р°РґР°С‡i");
     break;
 }
 }
