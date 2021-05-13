864b0a01b97e2d76d329fe6daaaf36e91535e6a8
Romadariychuck <78026310+Romadariychuck@users.noreply.github.com>
2021-05-13 13:02:28 +0300

    123

---
+++ z6.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int main(void){
+    int n = 1;int m = 1; int j; int i;
+	setlocale(LC_CTYPE,"ua");
+printf("\n\n¬ведiть к-сть р€дкiв: ");
+scanf("%d",&n);
+printf("\n\n¬ведiть к-сть стовпцiв: ");
+scanf("%d",&m);
+i = n;int colCount = n; j = m; int NextCount = m;
+float C[i][j];
+for(int i = 0; i < colCount; i ++){ 
+for(int j = 0; j < NextCount; j ++)
+   if (i<j) C[i][j]= pow(i,2)+pow(j,2)+2;
+    if (i==j) C[i][j]=(1)/(i+j);
+    if (i>j) C[i][j] = sin(i)+sin(j);
+}
+printf("\n\nƒвухмiрний масив C: \n");
+for (int i = 0; i < colCount; i++){
+for (int j = 0; j < NextCount; j++)
+printf("a[%d][%d]=%f\t", i, j, C[i][j]); printf("\n");}
+}
